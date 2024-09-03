15125fd1b4970d427c0af36286f842cec3d23138
Sandro Pedrett <sandro.pedrett@hotmail.com>
2024-09-03 21:50:21 +0000

    Fixed yocto initial build configuration

--- setup.sh
+++ setup.sh
@@ -2,5 +2,18 @@
 
 . sources/meta-imx/tools/imx-setup-release.sh
 
-echo "" >> $BUILD_DIR/conf/bblayers.conf
-echo "BBLAYERS += \"\${BSPDIR}/sources/meta-qlocky\"" >> $BUILD_DIR/conf/bblayers.conf
+unhook_layer() {
+    layer=$1
+    line="BBLAYERS += \"\${BSPDIR}/sources/$layer\""
+
+    # Change settings according to environment
+    sed "s,$line,,g" -i $BUILD_DIR/conf/bblayers.conf
+}
+
+# Some layers have been deleted from default imx setup.
+unhook_layer meta-nxp-demo-experience
+
+# add custom Qlocky layers
+echo "# Qlocky Release layers" >> $BUILD_DIR/conf/bblayers.conf
+hook_in_layer meta-qlocky
+
